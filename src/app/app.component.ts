23923cd804afaffa916b4a05b38df96a6b91e02b
Soriga Andrei <andrei.soriga@gmail.com>
2017-07-04 21:26:21 +0300

    Added upload fucntionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MdSnackBar } from '@angular/material';
+import { FileItem, FileLikeObject, FileUploader, ParsedResponseHeaders } from 'ng2-file-upload';
+import { Action } from 'rxjs/scheduler/Action';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    hasBaseDropZoneOver = false;
+    uploadedFileName = null;
+    selectedIndex = 0;
+    uploader: FileUploader;
+
+    constructor(public snackBar: MdSnackBar) { }
+
+    ngOnInit(): void {
+
+        this.uploader = new FileUploader({
+            url: 'http://127.0.0.1:5000/uploads',
+            headers: [{name: 'Accept', value: 'application/json'}],
+            // https://www.sitepoint.com/web-foundations/mime-types-complete-list/
+            allowedMimeType: ['application/zip'],
+            // maxFileSize: 1024*1024,
+            // autoUpload: true,
+        });
+        this.uploader.onErrorItem = (item, response, status, headers) => this.onErrorItem(item, response, status, headers);
+        this.uploader.onSuccessItem = (item, response, status, headers) => this.onSuccessItem(item, response, status, headers);
+        this.uploader.onWhenAddingFileFailed = (item, filter, options) => this.onWrongItem(item, filter, options);
+
+    }
+
+    onSuccessItem(item: FileItem, response: string, status: number, headers: ParsedResponseHeaders): any {
+        const data = JSON.parse(response);
+
+        // set the filename
+        this.uploadedFileName = data.filename;
+
+        // move to the second tab
+        this.selectedIndex = 1;
+    }
+
+    onErrorItem(item: FileItem, response: string, status: number, headers: ParsedResponseHeaders): any {
+        const data = JSON.parse(response);
+        this.snackBar.open(data.error, 'Close', {duration: 3000, extraClasses: ['snack-error']});
+    }
+
+    onWrongItem(item: FileLikeObject, filter: any, options: any) {
+        this.snackBar.open(`File '${item.name}' is not allowed.`, 'Close', {duration: 3000, extraClasses: ['snack-error']});
+    }
+
+    public fileOverBase(e: any): void {
+        this.hasBaseDropZoneOver = e;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import 'hammerjs';
+
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdIconModule, MdTabsModule, MdSnackBarModule} from '@angular/material';
+import {FileDropDirective, FileSelectDirective} from 'ng2-file-upload';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FileDropDirective,
+        FileSelectDirective
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdCheckboxModule,
+        MdTabsModule,
+        MdIconModule,
+        MdSnackBarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
